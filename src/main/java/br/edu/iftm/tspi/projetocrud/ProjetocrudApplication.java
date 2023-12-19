8047ac57e857b8017674964c050307dec644f885
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2023-12-19 00:15:31 -0300

    Create the project

---
+++ src/main/java/br/edu/iftm/tspi/projetocrud/ProjetocrudApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.projetocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/edu/iftm/tspi/projetocrud/dto/CadastroDTO.java
@@ -0,0 +1,15 @@
+package br.edu.iftm.tspi.projetocrud.dto;
+
+import lombok.Data;
+
+@Data
+public class CadastroDTO {
+    
+    private String nome;
+
+    private String nick;
+
+    private String jogo;
+
+    private String contato;
+}
